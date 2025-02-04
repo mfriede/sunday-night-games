0711da57ed90c4968df85982354a84a0fd123fbf
mfriede <mfriede@gmail.com>
2025-02-04 13:54:16 -0500

    SSL config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
+        destination: "https://sundaynightgames.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
+
+
 
 export default nextConfig;
